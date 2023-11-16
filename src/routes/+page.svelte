ce2e21817d2656d991ff986bebf459f696541eec
youssra <youssramortai01@gmail.com>
2023-11-16 15:30:17 +0100

    voorbeeld kaartje klaar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+</script>
+
+<div class="wrapper">
+    <img src="https://i.pinimg.com/564x/6c/fa/11/6cfa118a8c8e55693d5cdd7ed95e8e95.jpg" alt="plant">
+    <article>
+        <h3>Plantisus</h3>
+        <div><span>icon</span><span>icon</span></div>
+    </article>
+</div>
+
+
+<style>
+
+    .wrapper{
+        display: flex;
+        flex-direction: column;
+        width: 15rem;
+        height: auto;
+        border: solid #4E7141 2px;
+        border-radius: 2px;
+    }
+
+    .wrapper img{
+        width: 15rem;
+        height: 15rem;
+    }
+    article{
+        background-color: #4E7141;
+        padding: 10px;
+    }
+
+    h3{
+        font-family: Arial, Helvetica, sans-serif;
+        color: aliceblue;
+        font-size: 24px;
+        margin-top: -0.3rem;
+    }
+
+    article div{
+        display: flex;
+        justify-content: space-between;
+        padding: 0rem 1rem 0rem 1rem;
+    }
+
+</style>
