b4e7d6196d509fa5d3b1919aee20d06482da4f7d
youssra <youssramortai01@gmail.com>
2023-11-22 14:36:47 +0100

    bruh

--- src/lib/utils/hygraph.js
+++ src/lib/utils/hygraph.js
@@ -1,5 +1,4 @@
-import { HYGRAPH_KEY } from '$env/static/private';
-import { HYGRAPH_URL } from '$env/static/private';
+import { HYGRAPH_KEY, HYGRAPH_URL } from '$env/static/private';
 
 import { GraphQLClient } from 'graphql-request';
 
